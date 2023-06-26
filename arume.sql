b36a2d94511667c24581049b2e4eb75f6a1c80dd
Arume13 <ajisuatmajipras2802@gmail.com>
2023-06-26 20:19:39 +0700

    update database

--- arume.sql
+++ arume.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Jun 2022 pada 12.02
--- Versi server: 10.4.22-MariaDB
--- Versi PHP: 7.4.27
+-- Generation Time: Jun 26, 2023 at 03:18 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tokokue`
+-- Database: `arume`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_barang`
+-- Table structure for table `tbl_barang`
 --
 
 CREATE TABLE `tbl_barang` (
@@ -35,19 +35,21 @@ CREATE TABLE `tbl_barang` (
   `deskripsi` mediumtext DEFAULT NULL,
   `gambar` text DEFAULT NULL,
   `berat` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
 
 --
--- Dumping data untuk tabel `tbl_barang`
+-- Dumping data for table `tbl_barang`
 --
 
 INSERT INTO `tbl_barang` (`id_barang`, `nama_barang`, `id_kategori`, `harga`, `deskripsi`, `gambar`, `berat`) VALUES
-(8, 'Kue Ultah Nigga Capucino Sanhok', 11, 200000, 'rasanya seperti mati lampu...', 'kue_ultah_wafer.jpg', 1000);
+(10, 'TV PANASAJA', 15, 1500000, 'INI TV BISA NONTON APA SAJA', 'tv1.png', 2000),
+(11, 'MAKBUK', 16, 5000000, 'MAKBUK PRO BANGET', 'makbuk.jpg', 1500),
+(12, 'IPONG 14 PROMAG', 17, 8900000, 'Ipong 14 promag mahal bgt gausah dibeli kalo gamampu ges', 'ipong.jpg', 1000);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_gambar`
+-- Table structure for table `tbl_gambar`
 --
 
 CREATE TABLE `tbl_gambar` (
@@ -55,41 +57,45 @@ CREATE TABLE `tbl_gambar` (
   `id_barang` int(11) NOT NULL,
   `ket` varchar(255) DEFAULT NULL,
   `gambar` text DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
 
 --
--- Dumping data untuk tabel `tbl_gambar`
+-- Dumping data for table `tbl_gambar`
 --
 
 INSERT INTO `tbl_gambar` (`id_gambar`, `id_barang`, `ket`, `gambar`) VALUES
 (27, 8, 'Uniforms', 'logouniforms.jpeg'),
 (26, 8, 'Mantap nggih', 'untadlogo.png'),
 (29, 9, 'pak luhut', 'opung.jpeg'),
-(30, 9, 'ertertrtrt', 'software_kaos.PNG');
+(30, 9, 'ertertrtrt', 'software_kaos.PNG'),
+(31, 10, 'tv', 'tv2.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_kategori`
+-- Table structure for table `tbl_kategori`
 --
 
 CREATE TABLE `tbl_kategori` (
   `id_kategori` int(11) NOT NULL,
   `nama_kategori` varchar(50) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
 
 --
--- Dumping data untuk tabel `tbl_kategori`
+-- Dumping data for table `tbl_kategori`
 --
 
 INSERT INTO `tbl_kategori` (`id_kategori`, `nama_kategori`) VALUES
-(10, 'Opera Cake'),
-(11, 'Blackforest');
+(15, 'Elektronik'),
+(16, 'Komputer'),
+(17, 'Gadget'),
+(18, 'hp'),
+(19, 'Baju');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_pelanggan`
+-- Table structure for table `tbl_pelanggan`
 --
 
 CREATE TABLE `tbl_pelanggan` (
@@ -98,23 +104,21 @@ CREATE TABLE `tbl_pelanggan` (
   `email` varchar(255) DEFAULT NULL,
   `password` text DEFAULT NULL,
   `foto` text DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
 
 --
--- Dumping data untuk tabel `tbl_pelanggan`
+-- Dumping data for table `tbl_pelanggan`
 --
 
 INSERT INTO `tbl_pelanggan` (`id_pelanggan`, `nama_pelanggan`, `email`, `password`, `foto`) VALUES
-(1, 'sianjay', 'sianjay@gmail.com', '1234', NULL),
-(2, 'Maman', 'maman@gmail.com', '1234', NULL),
-(4, 'Gideon', 'gideon@gmail.com', '12345', NULL),
-(5, 'tester', 'tester@gmail.com', 'password', NULL),
-(6, 'joki', 'joki@gmail.com', '12345', NULL);
+(8, 'kepo', 'kepo123@mail.com', 'kepo123', NULL),
+(9, 'qwe', 'qwerty@mail.com', 'qwerty', NULL),
+(7, 'kepokamuh', 'demo123@mail.com', 'demo123', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_rekening`
+-- Table structure for table `tbl_rekening`
 --
 
 CREATE TABLE `tbl_rekening` (
@@ -122,19 +126,19 @@ CREATE TABLE `tbl_rekening` (
   `nama_bank` varchar(25) DEFAULT NULL,
   `no_rek` varchar(25) DEFAULT NULL,
   `atas_nama` varchar(25) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
 
 --
--- Dumping data untuk tabel `tbl_rekening`
+-- Dumping data for table `tbl_rekening`
 --
 
 INSERT INTO `tbl_rekening` (`id_rekening`, `nama_bank`, `no_rek`, `atas_nama`) VALUES
-(1, 'BRI', '5434-4382-3434-4345', 'MOH. AGUNG NURSALIM');
+(1, 'BCA', '6281836614', 'Hana Gracie Rosaleen');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_rinci_transaksi`
+-- Table structure for table `tbl_rinci_transaksi`
 --
 
 CREATE TABLE `tbl_rinci_transaksi` (
@@ -142,10 +146,10 @@ CREATE TABLE `tbl_rinci_transaksi` (
   `no_order` varchar(25) DEFAULT NULL,
   `id_barang` int(11) DEFAULT NULL,
   `qty` int(11) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
 
 --
--- Dumping data untuk tabel `tbl_rinci_transaksi`
+-- Dumping data for table `tbl_rinci_transaksi`
 --
 
 INSERT INTO `tbl_rinci_transaksi` (`id_rinci`, `no_order`, `id_barang`, `qty`) VALUES
@@ -154,12 +158,19 @@ INSERT INTO `tbl_rinci_transaksi` (`id_rinci`, `no_order`, `id_barang`, `qty`) V
 (28, '20220623EZNAYZAQ', 8, 1),
 (29, '202206235CDEUAC0', 8, 1),
 (30, '20220624M6RYOZUY', 9, 2),
-(31, '20220624M6RYOZUY', 8, 1);
+(31, '20220624M6RYOZUY', 8, 1),
+(32, '20230622CSAPVEWY', 8, 1),
+(33, '20230622OYRGCY6C', 11, 1),
+(34, '20230622QYBRZ23X', 12, 1),
+(35, '20230623B7AHBZGK', 12, 1),
+(36, '20230625YSSIW4HJ', 11, 1),
+(37, '20230626CZOST0XB', 12, 1),
+(38, '202306264BHVW95P', 12, 1);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_setting`
+-- Table structure for table `tbl_setting`
 --
 
 CREATE TABLE `tbl_setting` (
@@ -168,19 +179,19 @@ CREATE TABLE `tbl_setting` (
   `lokasi` int(11) DEFAULT NULL,
   `alamat_toko` text DEFAULT NULL,
   `no_telpon` varchar(15) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
 
 --
--- Dumping data untuk tabel `tbl_setting`
+-- Dumping data for table `tbl_setting`
 --
 
 INSERT INTO `tbl_setting` (`id`, `nama_toko`, `lokasi`, `alamat_toko`, `no_telpon`) VALUES
-(1, 'Echax Cake', 329, 'Jl. Imam Bonjol No.118, Kota Poso', '0823532455245');
+(1, 'Arume Shop', 154, 'Jl. Kepo banget sih kamuh', '08123456789');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_transaksi`
+-- Table structure for table `tbl_transaksi`
 --
 
 CREATE TABLE `tbl_transaksi` (
@@ -208,10 +219,10 @@ CREATE TABLE `tbl_transaksi` (
   `no_rek` varchar(25) DEFAULT NULL,
   `status_order` int(1) DEFAULT NULL,
   `no_resi` varchar(25) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
 
 --
--- Dumping data untuk tabel `tbl_transaksi`
+-- Dumping data for table `tbl_transaksi`
 --
 
 INSERT INTO `tbl_transaksi` (`id_transaksi`, `id_pelanggan`, `no_order`, `tgl_order`, `nama_penerima`, `hp_penerima`, `provinsi`, `kota`, `alamat`, `kode_pos`, `expedisi`, `paket`, `estimasi`, `ongkir`, `berat`, `grand_total`, `total_bayar`, `status_bayar`, `bukti_bayar`, `atas_nama`, `nama_bank`, `no_rek`, `status_order`, `no_resi`) VALUES
@@ -219,12 +230,19 @@ INSERT INTO `tbl_transaksi` (`id_transaksi`, `id_pelanggan`, `no_order`, `tgl_or
 (21, 1, '20220623VBQA8QGL', '2022-06-23', 'Rifki', '0857343434365', 'Jawa Barat', '24', 'Jl.Soetomo', '94141', 'jne', 'OKE', '3-4 Hari', 40000, 1000, 200000, 240000, 0, NULL, NULL, NULL, NULL, 0, NULL),
 (22, 1, '20220623EZNAYZAQ', '2022-06-23', 'anjay', '085734343436545', 'Kalimantan Tengah', '296', 'Jl.Jaelangkara', '94141', 'jne', 'OKE', '4-5 Hari', 57000, 1000, 200000, 257000, 0, NULL, NULL, NULL, NULL, 0, NULL),
 (23, 4, '202206235CDEUAC0', '2022-06-23', 'Rafi', '083757034915', 'Sulawesi Tenggara', '494', 'Jl.Jaelangkara', '94141', 'jne', 'OKE', '3-4 Hari', 48000, 1000, 200000, 248000, 1, 'images_(8).jpeg', 'Rafi', 'BRI', '423788-78876-788', 2, '4353434'),
-(24, 5, '20220624M6RYOZUY', '2022-06-24', 'tester', '08573434343234', 'DI Yogyakarta', '39', 'jl.Sepi', '96343', 'jne', 'OKE', '3-4 Hari', 123000, 3000, 800000, 923000, 1, 'WhatsApp_Image_2022-06-23_at_12_46_082.jpeg', 'tester', 'BRI', '73454-42435-3434-4345665', 3, '20220624M6Rrtrtery');
+(24, 5, '20220624M6RYOZUY', '2022-06-24', 'tester', '08573434343234', 'DI Yogyakarta', '39', 'jl.Sepi', '96343', 'jne', 'OKE', '3-4 Hari', 123000, 3000, 800000, 923000, 1, 'WhatsApp_Image_2022-06-23_at_12_46_082.jpeg', 'tester', 'BRI', '73454-42435-3434-4345665', 3, '20220624M6Rrtrtery'),
+(25, 6, '20230622CSAPVEWY', '2023-06-22', 'fadil', '123456', 'DKI Jakarta', '152', 'kepo', '12345', 'jne', 'REG', '2-3 Hari', 50000, 1000, 200000, 250000, 1, 'hyewon.jpg', 'fadil', 'puki', '3820183021', 2, '64123'),
+(26, 6, '20230622OYRGCY6C', '2023-06-22', 'akusendiri', '098773892', 'DKI Jakarta', '152', 'kepobgt da', '12345', 'jne', 'CTC', '1-2 Hari', 20000, 1500, 5000000, 5020000, 0, NULL, NULL, NULL, NULL, 0, NULL),
+(27, 6, '20230622QYBRZ23X', '2023-06-22', 'kepobanget', '12038905', 'DKI Jakarta', '154', 'kepo kamu', '12346', 'jne', 'CTC', '1-2 Hari', 10000, 1000, 8900000, 8910000, 1, 'Gambar_Buku.png', 'fadil', 'ABV', '0091283908', 3, '12390'),
+(28, 6, '20230623B7AHBZGK', '2023-06-23', 'fadil wibu', '08321789124', 'Bali', '17', 'kepo kamuuh', '65430', 'jne', 'OKE', '3-6 Hari', 30000, 1000, 8900000, 8930000, 1, 'Gambar_Buku1.png', 'fadil wibu', 'fadil bank', '9786780934590', 2, '8908690'),
+(29, 7, '20230625YSSIW4HJ', '2023-06-25', 'kepobanget', '01232790124', 'DKI Jakarta', '154', 'isdfg', '21355', 'jne', 'CTC', '1-2 Hari', 20000, 1500, 5000000, 5020000, 1, 'Gambar_Buku2.png', 'qweqwe', 'qwet', '1231245', 3, '123135'),
+(30, 8, '20230626CZOST0XB', '2023-06-26', 'qwer', '674556234', 'DKI Jakarta', '154', 'sadfg', '2134', 'tiki', 'REG', '2 Hari', 9000, 1000, 8900000, 8909000, 1, 'Gambar_Buku3.png', 'weqr', 'wqrt', '1235213', 3, '321415'),
+(31, 7, '202306264BHVW95P', '2023-06-26', 'fadilah', '081234567789', 'Jawa Timur', '441', 'jalan sumenep no. 3', '21315', 'jne', 'OKE', '4-7 Hari', 23000, 1000, 8900000, 8923000, 1, '2-12-154x300.jpg', 'fadilah', 'BRI', '2138859182739', 3, '2135123');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tbl_user`
+-- Table structure for table `tbl_user`
 --
 
 CREATE TABLE `tbl_user` (
@@ -232,10 +250,10 @@ CREATE TABLE `tbl_user` (
   `nama_user` varchar(25) DEFAULT NULL,
   `username` varchar(25) DEFAULT NULL,
   `password` varchar(25) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
 
 --
--- Dumping data untuk tabel `tbl_user`
+-- Dumping data for table `tbl_user`
 --
 
 INSERT INTO `tbl_user` (`id_user`, `nama_user`, `username`, `password`) VALUES
@@ -247,107 +265,107 @@ INSERT INTO `tbl_user` (`id_user`, `nama_user`, `username`, `password`) VALUES
 --
 
 --
--- Indeks untuk tabel `tbl_barang`
+-- Indexes for table `tbl_barang`
 --
 ALTER TABLE `tbl_barang`
   ADD PRIMARY KEY (`id_barang`) USING BTREE;
 
 --
--- Indeks untuk tabel `tbl_gambar`
+-- Indexes for table `tbl_gambar`
 --
 ALTER TABLE `tbl_gambar`
   ADD PRIMARY KEY (`id_gambar`) USING BTREE;
 
 --
--- Indeks untuk tabel `tbl_kategori`
+-- Indexes for table `tbl_kategori`
 --
 ALTER TABLE `tbl_kategori`
   ADD PRIMARY KEY (`id_kategori`) USING BTREE;
 
 --
--- Indeks untuk tabel `tbl_pelanggan`
+-- Indexes for table `tbl_pelanggan`
 --
 ALTER TABLE `tbl_pelanggan`
   ADD PRIMARY KEY (`id_pelanggan`) USING BTREE;
 
 --
--- Indeks untuk tabel `tbl_rekening`
+-- Indexes for table `tbl_rekening`
 --
 ALTER TABLE `tbl_rekening`
   ADD PRIMARY KEY (`id_rekening`) USING BTREE;
 
 --
--- Indeks untuk tabel `tbl_rinci_transaksi`
+-- Indexes for table `tbl_rinci_transaksi`
 --
 ALTER TABLE `tbl_rinci_transaksi`
   ADD PRIMARY KEY (`id_rinci`) USING BTREE;
 
 --
--- Indeks untuk tabel `tbl_setting`
+-- Indexes for table `tbl_setting`
 --
 ALTER TABLE `tbl_setting`
   ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
--- Indeks untuk tabel `tbl_transaksi`
+-- Indexes for table `tbl_transaksi`
 --
 ALTER TABLE `tbl_transaksi`
   ADD PRIMARY KEY (`id_transaksi`) USING BTREE;
 
 --
--- Indeks untuk tabel `tbl_user`
+-- Indexes for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
   ADD PRIMARY KEY (`id_user`) USING BTREE;
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `tbl_barang`
+-- AUTO_INCREMENT for table `tbl_barang`
 --
 ALTER TABLE `tbl_barang`
-  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT untuk tabel `tbl_gambar`
+-- AUTO_INCREMENT for table `tbl_gambar`
 --
 ALTER TABLE `tbl_gambar`
-  MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
--- AUTO_INCREMENT untuk tabel `tbl_kategori`
+-- AUTO_INCREMENT for table `tbl_kategori`
 --
 ALTER TABLE `tbl_kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
--- AUTO_INCREMENT untuk tabel `tbl_pelanggan`
+-- AUTO_INCREMENT for table `tbl_pelanggan`
 --
 ALTER TABLE `tbl_pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT untuk tabel `tbl_rekening`
+-- AUTO_INCREMENT for table `tbl_rekening`
 --
 ALTER TABLE `tbl_rekening`
   MODIFY `id_rekening` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `tbl_rinci_transaksi`
+-- AUTO_INCREMENT for table `tbl_rinci_transaksi`
 --
 ALTER TABLE `tbl_rinci_transaksi`
-  MODIFY `id_rinci` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id_rinci` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
--- AUTO_INCREMENT untuk tabel `tbl_transaksi`
+-- AUTO_INCREMENT for table `tbl_transaksi`
 --
 ALTER TABLE `tbl_transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
--- AUTO_INCREMENT untuk tabel `tbl_user`
+-- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
